7d34b28ad5f3f277798ac884b8b8d76c0a858072
Adam Tak <adam.tak2112@gmail.com>
2021-02-22 12:50:57 -0700

    create card models

--- cards/game/models.py
+++ cards/game/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BlackCard(models.Model):
+    text = models.TextField()
+    pick = models.IntegerField()
+    draw = models.IntegerField()
+
+class WhiteCard(models.Model):
+    text = models.TextField()
